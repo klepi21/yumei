a004342289df62b35628b612ae51cfdb1ec06295
Dinos Lepidas <kolepidas@gmail.com>
2026-01-31 01:27:26 +0200

    fix: allow utfs.io remote pattern in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'api.together.ai',
       },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
     ],
   },
 };
